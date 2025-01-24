8d05f4abbd9f4c7b6bcc5dd4503e6769d934b9d3
kingmikomv <arifarhan1602@gmail.com>
2025-01-24 13:51:46 +0700

    Update S

--- resources/views/components/dcore/script.blade.php
+++ resources/views/components/dcore/script.blade.php
@@ -21,6 +21,18 @@
   <script src="{{asset('dbs/assets/js/custom.js')}}"></script>
   <script src="https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/679337e13a842732607436a7/1iibh4ik6';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
 </body>
 </html>
